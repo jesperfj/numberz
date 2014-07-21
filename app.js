596c665806a3a2758e8f19de2aef72791f7f2962
Jesper Joergensen <jesper@heroku.com>
2014-07-20 23:09:01 -0700

    now functional

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var logfmt = require("logfmt");
 var PW = require('png-word');
 var pngword = PW(PW.GRAY);
+var request = require('request');
 
 var app = express();
 
@@ -12,10 +13,23 @@ app.get('/', function(req, res) {
 });
 
 app.get('/:clip/count.png', function(req, res) {
-  pngword.createPNG('abcd', function(pngnum) {
-    res.set('Content-Type', 'image/png');
-    res.send(pngnum);
-  });
+  request('https://dataclips.heroku.com/'+req.param('clip')+'.json', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = JSON.parse(body);
+      console.log(data.values.length);
+      pngword.createPNG(data.values.length, function(pngnum) {
+        res.set('Content-Type', 'image/png');
+        res.send(pngnum);
+      });
+    } else {
+      console.log(response.statusCode);
+      console.log(error);
+      pngword.createPNG('err', function(pngnum) {
+        res.set('Content-Type', 'image/png');
+        res.send(pngnum);
+      });
+    }
+  })
 });
 
 
