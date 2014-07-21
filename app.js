a810bdab1c410af4c91b62b0b58faea068066e32
Jesper Joergensen <jesper@heroku.com>
2014-07-20 22:33:55 -0700

    a working test png

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var logfmt = require("logfmt");
+var PW = require('png-word');
+var pngword = PW(PW.GRAY);
+
 var app = express();
 
 app.use(logfmt.requestLogger());
@@ -7,6 +10,14 @@ app.use(logfmt.requestLogger());
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
+
+app.get('/:clip/count.png', function(req, res) {
+  pngword.createPNG('abcd', function(pngnum) {
+    res.set('Content-Type', 'image/png');
+    res.send(pngnum);
+  });
+});
+
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
